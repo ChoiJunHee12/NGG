16744a1d567c07a5c949b470dea6f292020de1c2
codechoo <cntjdgh98@naver.com>
2024-08-06 17:45:46 +0900

    Create IntresVO.java

---
+++ mydream/src/main/java/kr/ict/mydream/vo/IntresVO.java
@@ -0,0 +1,23 @@
+package kr.ict.mydream.vo;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter
+@Setter
+@Alias("intresvo")
+public class IntresVO {
+    private int intno;
+    private int memno;
+    private String inttypecd;
+    private String aifeedbk;
+    private String cnsfeedbk;
+    private float cnsscore;
+    private Date itenddt;
+    private float reviewscore;
+    private Date credt;
+    private Date upddt;
+}
